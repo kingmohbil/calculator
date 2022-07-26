fe6ee240f285866ff7071c685b2cc2c32861d532
Mohammad <kingwazzan2@gmail.com>
2022-07-26 18:47:13 +0300

    in short the files contain a little logic in js
    the edits was a function called process added to do the logic in all the operators to avoid the repetetion of the code added more varibles to control the calculator.

--- index.js
+++ index.js
@@ -1,25 +1,69 @@
-function add(a, b) {return a+b;}
-function divide(a, b){
-    if(b!==0){
-    return a/b;}
-    else{
-        alert('Error:');
+function add (a, b){ 
+    if (b === undefined )return +a; 
+    else return +a + +b;
+}
+
+
+function subtract(a, b){
+    if(b === undefined) return +a;
+    else return +a - +b;
     }
 
-    }
-function multiply(a, b){return a*b;}
-function subtract(a, b){return a-b;}
-function operate(Opreator, a, b){
-    switch(Opreator){
-        
-        case '+':return add(a,b);
-        case '-':return subtract(a,b);
-        case '*': return multiply(a,b);
-        case '/': return divide(a,b);
-        case '':return 0;
+
+function multiply(a, b){
+    if(b === undefined) return +a;
+    else return +a * +b;
+}
+
+
+function divide(a, b){
+    if(b===undefined) return +a;
+    else return +a / +b;
+}
+
+function operate(opreator, valueOne, valueTwo){
+    switch(opreator){
+        case '+': return add(valueOne, valueTwo);
+        case '-': return subtract(valueOne, valueTwo);
+        case '*': return multiply(valueOne, valueTwo);
+        case '/': return divide(valueOne, valueTwo);   
+        case '': return undefined;
     }
 }
 
+function process(ope, sign='') {
+    result = operate(opreator, number1, number2);
+    topScreen.textContent = (result + sign);
+    opreator=ope;
+    for ( let element of numbers){
+        element.onclick=()=>{ number2+= element.innerHTML;};
+    }
+    result = operate(opreator, number1, number2);    
+    number2 = ''; 
+    number1 = result ; 
+    topScreen.innerHTML = result;
+}
+let number1 = '';       // holds the first value in the calculator 
+let number2;              // holds the second value in the calculator it's been set to undefined
+                    // so that it can't crash the calculator with false value like zero
+let opreator ='';  
+let result ='';
+
+const topScreen = document.querySelector('.top-screen');
+topScreen.innerHTML = '';
+
+
+const numbers = document.getElementsByClassName('num');
+for(let element of numbers){
+    element.onclick=()=>{number1 += element.innerHTML;};
+}
+
+document.getElementById('add').onclick=()=>{
+    opreator = '+';
+    process(opreator, '+');
+}
+
+/*
 
 let text='';
 let result=0;
@@ -77,4 +121,4 @@ document.querySelector('#equal').onclick=()=>{
     display.innerHTML=result;
     text='';
 
-}
+}*/
