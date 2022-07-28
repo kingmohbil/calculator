4f74cf35e7f451113287cfbf39d66906a64a7f82
Mohammad Alwazzan <kingwazzan2@gmail.com>
2022-07-28 06:35:57 +0300

    the html edited so adds a two new features
    the html contain the backspace button and changes the position of the clear div , also contains the decimal point, the java script edited so it can perform the wanted logic from the html edits, add a google font style to show the backspace icon

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let number1 = '';       // holds the first value in the calculator
 let number2 = '';       // holds the second value in the calculator it's been set to undefined so that it can't crash the calculator with false value like zero
 let opreator ='';  
 let result ='';
-
+let point;
 const topScreen = document.querySelector('.top-screen');
 topScreen.innerHTML = 0;
 
@@ -14,9 +14,16 @@ const numbers = document.getElementsByClassName('num');
 
 
 for(let element of numbers){
-    element.onclick=()=>{number1 += element.innerHTML;
+    element.onclick=()=>{
+        if(element.innerHTML == '.' ){
+            if(!number1.includes('.'))
+            number1 += element.innerHTML;
+        }
+    else
+    number1 += element.innerHTML;
     topScreen.innerHTML=result;
-    bottomScreen.innerHTML=number1};
+    bottomScreen.innerHTML=number1
+    point=0;};
 }
 
 document.getElementById('add').onclick = ()=>{process('+');}   
@@ -37,9 +44,16 @@ document.getElementById('clear').onclick =()=>{
     topScreen.innerHTML=result;
     bottomScreen.innerHTML=number1
     for(let element of numbers){
-        element.onclick=()=>{number1 += element.innerHTML;
+        element.onclick=()=>{
+            if(element.innerHTML == '.' ){
+                if(!number1.includes('.'))
+                number1 += element.innerHTML;
+            }
+        else
+        number1 += element.innerHTML;
         topScreen.innerHTML=result;
-        bottomScreen.innerHTML=number1};
+        bottomScreen.innerHTML=number1
+    point=0;};
     }
 }
 
@@ -80,13 +94,33 @@ function process(ope){
     topScreen.innerHTML=result;
     bottomScreen.innerHTML='';
     for(let element of numbers){
-        element.onclick=()=>{number2 += element.innerHTML
+        element.onclick=()=>{
+            if(element.innerHTML == '.' ){
+                if(!number2.includes('.'))
+                number2 += element.innerHTML;
+            }
+        else
+            number2 += element.innerHTML
             topScreen.innerHTML=result;
-            bottomScreen.innerHTML=number2};
+            bottomScreen.innerHTML=number2}
+            point=1;
     }
     result = operate(opreator,number1, number2);
     topScreen.innerHTML = result;
     number1=result;
     number2='';
     opreator=ope;
+}
+
+document.getElementById('back').onclick = ()=>{
+    if(point === 0){
+        number1 = number1.slice(0,number1.length-1);
+        topScreen.innerHTML=result;
+        bottomScreen.innerHTML=number1;
+    }
+    else if(point === 1){
+        number2 = number2.slice(0,number2.length-1);
+        topScreen.innerHTML=result;
+        bottomScreen.innerHTML=number2;
+    }
 }
